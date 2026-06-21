3c90278d335a7f0c8faef6ea51b7b471cef5a18d
Franco Romero <panco.2010@hotmail.com>
2026-06-21 17:48:59 -0300

    UI: Configuracion de metodo main para iniciar MenuPrincipal

--- src/main/java/com/mycompany/foodstore/FoodStore.java
+++ src/main/java/com/mycompany/foodstore/FoodStore.java
@@ -1,7 +1,14 @@
 package com.mycompany.foodstore;
+
+import com.mycompany.foodstore.ui.MenuPrincipal;
+
 public class FoodStore {
 
     public static void main(String[] args) {
-        
+        System.out.println("=== Iniciando Sistema FoodStore ===");
+
+        MenuPrincipal menu = new MenuPrincipal();
+
+        menu.iniciar(); 
     }
 }
